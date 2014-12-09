a231d404ea6c564786286c9efe945ad3bca1ad08
boyofthunder <boyofthunder2693@gmail.com>
2014-12-09 17:08:04 +0700

    active admin + design
    reupload

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,3 +6,12 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( bootstrap.css )
+Rails.application.config.assets.precompile += %w( popuo-box.css )
+Rails.application.config.assets.precompile += %w( owl.carousel.css )
+Rails.application.config.assets.precompile += %w( style.css )
+Rails.application.config.assets.precompile += %w( jquery.min.js )
+Rails.application.config.assets.precompile += %w( easing.js )
+Rails.application.config.assets.precompile += %w( move-top.js )
+Rails.application.config.assets.precompile += %w( responsiveslides.min.js )
+Rails.application.config.assets.precompile += %w( owl.carousel.js )
